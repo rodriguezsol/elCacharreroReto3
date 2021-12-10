8ee37845132f13058d4f1b6c22c55332f7fe4086
rodriguezsol <marisol.rodriguez.mt@correo.usa.edu.co>
2021-12-09 22:02:55 -0500

    Para Mastertech

---
+++ src/main/java/com/usa/ciclo4/elCacharrero/repository/OrderRepository.java
@@ -0,0 +1,48 @@
+package com.usa.ciclo4.elCacharrero.repository;
+
+
+import com.usa.ciclo4.elCacharrero.crudRepository.OrderCrudRepository;
+import com.usa.ciclo4.elCacharrero.model.Order;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author solecito
+ */
+
+@Repository
+public class OrderRepository {
+
+    @Autowired
+    private OrderCrudRepository orderCrudRepository;
+
+    public List<Order> getAll() {
+        return (List<Order>) orderCrudRepository.findAll();
+    }
+
+    public Optional<Order> getOrder(Integer id) {
+        return orderCrudRepository.findById(id);
+    }
+
+    public Order save(Order order) {
+        return orderCrudRepository.save(order);
+    }
+
+    public void update(Order order) {
+        orderCrudRepository.save(order);
+    }
+
+    public void delete(Order order) {
+        orderCrudRepository.delete(order);
+    }
+    
+    public Optional<Order> lastUserId(){
+        return orderCrudRepository.findTopByOrderByIdDesc();
+    }
+    public List<Order> findByZone(String zona) {
+        return orderCrudRepository.findUserByZone(zona);
+    }
+}
